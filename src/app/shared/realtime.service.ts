06d96a31874f3bb89d4aa4e2a942c0653756eaf8
Zhentian Wan <answer881215@gmail.com>
2016-10-01 20:53:52 +0300

    add db script code

--- src/app/shared/realtime.service.ts
+++ src/app/shared/realtime.service.ts
@@ -7,7 +7,7 @@ export class RealtimeService {
   constructor() {
 
     // Initialize Firebase
-    var config = {
+    const config = {
       apiKey: "AIzaSyDiS_LY_Sz9Go1KxvDCaF4vAIuhvjeQf2k",
       authDomain: "angular2-firebase-961ae.firebaseapp.com",
       databaseURL: "https://angular2-firebase-961ae.firebaseio.com",
