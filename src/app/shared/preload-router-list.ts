5b68e9f777bb1d33d7514017f9343ea20923a0ba
Zhentian Wan <answer881215@gmail.com>
2016-10-13 16:06:05 +0300

    preload

---
+++ src/app/shared/preload-router-list.ts
@@ -0,0 +1,9 @@
+
+import {PreloadingStrategy, Route} from "@angular/router";
+import {Observable} from "rxjs";
+export class PreloadSelectedModuledsList implements PreloadingStrategy {
+  preload(route: Route, load: Function): Observable<any> {
+    return route.data.preload ? load() : Observable.of(null);
+  }
+
+}
