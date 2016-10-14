494c35b9fb3cbb6d05cacdf4e45a0ac373ba4fd4
Zhentian Wan <answer881215@gmail.com>
2016-10-14 23:33:50 +0300

    pagination

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -34,7 +34,60 @@ export class CourseService {
     return this.findCourseByUrl(courseUrl)
       .filter(course => !!course)
       .map((course) => course.$key)
-      .switchMap((courseKey) => this.db.list(`lessonsPerCourse/${courseKey}`));
+      .switchMap((courseKey) => this.db.list(`lessonsPerCourse/${courseKey}`, {
+        query: {
+          limitToFirst: 3,
+          orderByKey: true
+        }
+      }));
+  }
+
+  findPreviousPageLessonsKey(courseUrl: string, end: Lesson, limit: number){
+    return this.findCourseByUrl(courseUrl)
+      .filter(c => !!c)
+      .switchMap((c) => this.db.list(`lessonsPerCourse/${c.$key}`, {
+        query: {
+          limitToLast: limit + 1,
+          orderByKey: true,
+          endAt: end.$key
+        }
+      }))
+      .map(lessons => lessons.slice(0, lessons.length-1));
+  }
+
+  findPreviousPageLessons(courseUrl: string, end: Lesson, limit: number){
+    return this.findPreviousPageLessonsKey(courseUrl, end, limit)
+      .map((lessonKeys) => lessonKeys
+        .map((lessonKey) => {
+          return this.db.object(`lessons/${lessonKey.$key}`)
+        }))
+      .flatMap((res) => {
+        return Observable.combineLatest(res);
+      });
+  }
+
+  findNextPageLessons(courseUrl: string, from: Lesson, limit: number): Observable<Lesson[]>{
+    return this.findNextPageLessonsKey(courseUrl, from, limit)
+      .map((lessonKeys) => lessonKeys
+        .map((lessonKey) => {
+          return this.db.object(`lessons/${lessonKey.$key}`)
+        }))
+      .flatMap((res) => {
+        return Observable.combineLatest(res);
+      });
+  }
+
+  findNextPageLessonsKey(courseUrl: string, from: Lesson, limit: number){
+    return this.findCourseByUrl(courseUrl)
+      .filter(c => !!c)
+      .switchMap((c) => this.db.list(`lessonsPerCourse/${c.$key}`, {
+        query: {
+          limitToFirst: limit + 1,
+          orderByKey: true,
+          startAt: from.$key
+        }
+      }))
+      .map(lessons => lessons.slice(1, lessons.length));
   }
 
   findAllCourseLessons(courseUrl) {
--- src/app/shared/preload-router-list.ts
+++ src/app/shared/preload-router-list.ts
@@ -3,7 +3,7 @@ import {PreloadingStrategy, Route} from "@angular/router";
 import {Observable} from "rxjs";
 export class PreloadSelectedModuledsList implements PreloadingStrategy {
   preload(route: Route, load: Function): Observable<any> {
-    return route.data.preload ? load() : Observable.of(null);
+    return route.data && route.data.preload ? load() : Observable.of(null);
   }
 
 }
