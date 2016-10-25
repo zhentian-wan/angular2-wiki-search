360c40012a97280fcac3a90956b863fcedfc1617
Zhentian Wan <answer881215@gmail.com>
2016-10-25 23:46:40 +0300

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule, JsonpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
@@ -24,6 +24,7 @@ import {PreloadSelectedModuledsList} from "./shared/preload-router-list";
     appRoutes,
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     JsonpModule,
     SharedServiceModule.forRoot(),
